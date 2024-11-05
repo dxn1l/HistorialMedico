39b953b124fd01bcbc9c028b34033ba36b38bbc9
dxn1l <danielmorenorey2012@gmail.com>
2024-11-05 17:38:40 +0100

    Added Login.screen

---
+++ app/src/main/java/com/example/historialmedico/Screen/MenuScreen.kt
@@ -0,0 +1,20 @@
+package com.example.historialmedico.Screen
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MenuScreen() {
+
+
+    Button (onClick = { /* Handle the button click */ }) {
+        Text("Click me")
+    }
+
+
+
+}
+
+
+
