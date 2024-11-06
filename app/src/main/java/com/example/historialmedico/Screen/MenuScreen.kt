d9bd073dc4e50738530b54b831989639cd0762c8
Arturo <artuloca@myuax.co>
2024-11-06 18:43:45 +0100

    bloquear agregar boton

--- app/src/main/java/com/example/historialmedico/Screen/MenuScreen.kt
+++ app/src/main/java/com/example/historialmedico/Screen/MenuScreen.kt
@@ -3,22 +3,26 @@ package com.example.historialmedico.Screen
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.AlertDialog
 import androidx.compose.ui.Modifier
+import com.example.historialmedico.DataBase.FirebaseHistorialRepository
 
 
 @Composable
-fun MenuScreen(onAddHistorialClick: () -> Unit , onViewHistorialClick: () -> Unit) {
+fun MenuScreen(historialRepository: FirebaseHistorialRepository,
+    onAddHistorialClick: () -> Unit ,
+    onViewHistorialClick: () -> Unit) {
 
     val auth = FirebaseAuth.getInstance()
     val email = auth.currentUser?.email ?: ""
     val userName = email.substringBefore("@")
-
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -32,7 +36,18 @@ fun MenuScreen(onAddHistorialClick: () -> Unit , onViewHistorialClick: () -> Uni
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = onAddHistorialClick) {
+        Button(onClick = {
+            historialRepository.getHistorial(
+                onSuccess = { historialList ->
+                    if (historialList.any { it.email == email }) {
+                        showDialog = true
+                    } else {
+                        onAddHistorialClick()
+                    }
+                },
+                onFailure = { /* Manejar error */ }
+            )
+        }) {
             Text(text = "Agregar Historial")
         }
 
@@ -42,11 +57,27 @@ fun MenuScreen(onAddHistorialClick: () -> Unit , onViewHistorialClick: () -> Uni
             Text(text = "Ver historial")
         }
 
-
-
-
+        if (showDialog) {
+            AlertDialog(
+                onDismissRequest = { showDialog = false },
+                title = { Text("Ya tienes un historial") },
+                text = { Text("Ya tienes un historial registrado, si deseas modificarlo puedes hacerlo desde la opción de ver historial") },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            showDialog = false
+                            onViewHistorialClick()
+                        }
+                    ) {
+                        Text("Ver historial")
+                    }
+                }
+            )
+        }
 
     }
+
+}
 
 
 
